1a17bf470f436568ee4e7b2b0ff78892a7021314
jwyen12 <jakewyen1@gmail.com>
2026-02-15 13:08:23 -0500

    queue done (pre debug)

--- queueArray/queue_using_array.cpp
+++ queueArray/queue_using_array.cpp
@@ -29,3 +29,41 @@ void queue::resize(){
     internal_queue = newArr;
 
 }
+
+
+void queue::enqueue(int val){
+    if(count == capacity) resize();
+
+    rear_index = (rear_index+1) % capacity;
+    internal_queue[rear_index] = val;
+    count++;
+}
+
+
+void queue::dequeue(){
+    if(count == 0) return;
+
+    count--;
+    front_index = (front_index+1) % capacity;
+}
+
+
+void queue::peek(){
+    if(count == 0){
+        std::cout << "The queue is empty\n";
+    }
+    else{
+        std::cout << internal_queue[front_index];
+    }
+}
+
+
+void queue::display(){
+    if(count == 0){
+        std::cout << "The queue is empty\n";
+    }
+    for(int i = 0; i < count; i++){
+        std::cout << internal_queue[(front_index + i) % capacity] << "<-";
+    }
+    std::cout << std::endl;
+}
